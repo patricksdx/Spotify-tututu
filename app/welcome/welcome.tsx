9032d740886d0243eab29602a4065ead184d8d6c
Netmario <crisdeivi2020@gmail.com>
2025-01-16 21:15:43 -0500

    CAMBIOS EN LA LINEA h1, titulo, saludando a patrick. -Cristhofer

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,6 +6,9 @@ export function Welcome() {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
+
+
+          <h1>HOLA PACHI COMO ESTOY</h1>
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
               src={logoLight}
@@ -42,6 +45,10 @@ export function Welcome() {
           </nav>
         </div>
       </div>
+
+      
+
+      
     </main>
   );
 }
